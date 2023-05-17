5871e21a0079032eb4ab38f8940232f020729d01
김혜영 <kimhyeyeong@gimhyeyeong-ui-MacBookAir.local>
2023-05-17 17:25:09 +0900

    new diary css

--- src/components/tag/TagButton.jsx
+++ src/components/tag/TagButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function TagButton({buttonText, onClick}) {
     return (
-        <button type="button" onClick={onClick} className=' rounded-t-md bg-red-100 w-full h-full'>
+        <button type="button" onClick={onClick} className=' rounded-t-md bg-red-100 w-36 h-10 text-lg text-gray-700'>
             {buttonText}
         </button>
     );
--- src/components/tag/TagInfo.jsx
+++ src/components/tag/TagInfo.jsx
@@ -22,35 +22,35 @@ export default function TagInfo({tagValue, setWeight, weight, setHeight, height,
     switch (tagValue) {
         case 2 : 
         return (
-            <div className='flex flex-col w-full h-full'>
-                <div >
-                    <label className='flex justify-center items-center w-full h-10 text-lg'>아기 몸무게 :</label>
-                    <div>
-                        <input type='text' onChange={handleChangeWeight}/> 
-                        <span>Kg</span>
+            <div className='flex flex-col w-full h-96 '>
+                <div className='w-full h-1/2' >
+                    <label className='flex justify-center items-center w-full h-10  border-b-2 border-red-100'>아기 몸무게 :</label>
+                    <div className='flex justify-center items-center w-full h-10 '>
+                        <input type='text' onChange={handleChangeWeight} className='w-1/3 h-10 text-lg'/> 
+                        <span className='text-base'>Kg</span>
                     </div>
                 </div>
-                <div>
-                    <label className='flex justify-center items-center w-full h-10 text-lg'>아기 키 :</label>
-                    <div>
-                        <input type='text' onChange={handleChangeHeight}/> 
-                        <span>cm</span>
+                <div className='w-full h-1/2'>
+                    <label className='flex justify-center items-center w-full h-10  border-b-2 border-red-100'>아기 키 :</label>
+                    <div className='flex justify-center items-center w-full h-10'>
+                        <input type='text' onChange={handleChangeHeight} className='w-1/3 h-10 text-lg'/> 
+                        <span className='tex-base'>cm</span>
                     </div>
                 </div>
             </div>
         )
         case 3 : 
         return (
-            <div className='flex flex-col w-full h-full' >
-                <label className='flex justify-center items-center w-full h-10 text-lg'>장보기 List</label>
-                <textarea type='text' onChange={handleChangeText} className='w-full h-[500px] text-lg' />
+            <div className='flex flex-col w-full h-96' >
+                <label className='flex justify-center items-center w-full h-10  border-b-2 border-red-100'>장보기 List</label>
+                <textarea type='text' onChange={handleChangeText} className='w-full h-full' />
             </div>
         )
         default :
         return (
-            <div className='flex flex-col w-full h-full'>
-                <label className='flex justify-center items-center w-full h-10 text-lg'>엄마와 아기의 일상을 기록해주세요~</label>
-                <textarea type='text' onChange={handleChangeContent} className='w-full h-[500px] text-lg'  />
+            <div className='flex flex-col w-full h-96'>
+                <label className='flex justify-center items-center w-full h-10  border-b-2 border-red-100'>엄마와 아기의 일상을 기록해주세요~</label>
+                <textarea type='text' onChange={handleChangeContent} className='w-full h-full p-2'  />
             </div>
         )
     
