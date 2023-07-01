3ebb429845da979940e3de0c1af695b22b82614f
hye-yeong <ghdwls69@naver.com>
2023-07-01 14:50:20 +0900

    수정

--- src/pages/DiaryList.jsx
+++ src/pages/DiaryList.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { loggedApi } from '../axios';
 import { Link } from 'react-router-dom';
@@ -26,13 +27,12 @@ export default function DiaryList() {
                 <ol className='flex flex-wrap w-full h-full p-20 list-decimal list-inside'>
                 {
                         diaries ? diaries.map((diary) => (
-                            
                             <Link to={`/diary/${diary.id}`} key={diary.id} >
-                                { 
-                                    diary.id % 2 === 1 && diary.id !== 1 
-                                    ? <li className='w-32 h-32 bg-red-300'></li>
-                                    : <li className='w-32 h-48 bg-blue-200'></li>
-                                }
+                                <li className={`flex flex-col items-center w-32 ${diary.id % 2 === 1 ? 'bg-blue-200' : 'bg-red-300'}  ${diary.id % 2 === 0 ? 'h-32' : 'h-48'}`}>
+                                    <img />
+                                    <span>{diary.title}</span>
+                                </li>
+                               
                                 {/* <li className={`flex flex-col items-center bg-green-200 ${diary.id % 2 === 0 ? 'h-32' : 'h-16'}`}>
                                    <img className='w-32 h-32 bg-red-200' />
                                     <span className='bg-yellow-100'>{diary.title}</span>
