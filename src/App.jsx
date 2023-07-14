e425b1405623c3068e3b46778910e8ba1e09c359
hye-yeong <ghdwls69@naver.com>
2023-07-14 17:01:27 +0900

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "./components/Navbar"
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
-
   return (
-    <div className=""> 
-      <Navbar />     
+    <div className="">
+      <Navbar />
       <Outlet />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Lottie from 'lottie-react'
+import babyCart from '../lottie/baby car.json'
+
+export default function Footer() {
+  return (
+    <div className="mt-40 relative  bg-[#5d8964]">
+      <Lottie
+        className="absolute w-72 h-72 z-10 -bottom-14 right-20"
+        animationData={babyCart}
+      />
+      <div className="absolute w-full h-40 bg-[#5d8964]">
+        <div className="flex justify-center gap-4 mt-32  text-white">
+          <p>고객센터</p>
+          <p>사업 제휴</p>
+          <p>광고 문의</p>
+          <p>베베(주)</p>
+          <p>사업자 등록번호: 123-45-678900</p>
+          <p>대표이사: 김 혜 영 </p>
+        </div>
+      </div>
+    </div>
+  )
+}
