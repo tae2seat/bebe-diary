902fe9cd60a9e11b7bf14172401574e4bd8ef82e
hye-yeong <ghdwls69@naver.com>
2023-07-15 15:58:02 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex-grow">
-        <Outlet />
-      </div>
+      <Outlet />
       <Footer />
     </div>
   )
