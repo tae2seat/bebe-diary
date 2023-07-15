5de12c54581bd727a9e5706542986c02d77df985
hye-yeong <ghdwls69@naver.com>
2023-07-15 15:50:14 +0900

    outlet size

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Outlet />
+      <div className="flex-grow">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   )
