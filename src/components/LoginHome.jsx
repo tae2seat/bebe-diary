d6497ba92bbe52a669d7e60554038420730035ac
hye-yeong <ghdwls69@naver.com>
2023-07-15 15:07:04 +0900

    Navbar 수정

--- src/components/LoginHome.jsx
+++ src/components/LoginHome.jsx
@@ -1,74 +1,66 @@
-import React, { useState } from 'react';
-import '../pages/Home.css';
-import BabyProfileCard from './Cards/BabyProfileCard';
-import ProfileCard from './Cards/ProfileCard';
-import { Link, useParams } from 'react-router-dom';
-import baby1 from '../images/baby_01.png';
-
+import React, { useState } from 'react'
+import '../pages/Home.css'
+import BabyProfileCard from './Cards/BabyProfileCard'
+import ProfileCard from './Cards/ProfileCard'
+import { Link, useParams } from 'react-router-dom'
+import baby1 from '../images/baby_01.png'
 
 export default function LoginHome() {
+  const [isSignUp, setIsSignUp] = useState(true)
 
-    const { id } = useParams()
+  const handleSignUpClick = () => {
+    setIsSignUp(true)
+  }
 
-    const [isSignUp, setIsSignUp] = useState(true)
+  const handleSignInClick = () => {
+    setIsSignUp(false)
+  }
 
-    const handleSignUpClick = () => {
-        setIsSignUp(true);
-      };
-    
-      const handleSignInClick = () => {
-        setIsSignUp(false);
-      };
-
-    return (
-        <div>
-            <nav className='flex justify-end gap-4 py-4 mr-8'>
-                <Link to='/new'>
-                    <span className='underline' >New Diary</span>
-                </Link>
-                <Link to='/diaries'>
-                    <span className='underline'>Diray List</span>
-                </Link>
-                <Link to={`/profile/${id}/edit`}>
-                    <span className='underline'>Profile Edit</span>
-                </Link>
-                <Link to={`/baby/${id}/register`}>
-                    <span className='underline'>Baby Profile register</span>
-                </Link>
-            </nav>
-            <div className='container'>
-                <div className='welcome'>
-                    <div 
-                        className={`pinkbox ${isSignUp ? 'signup' : 'signin'}`}
-                        style={{ transform: isSignUp ? 'translateX(80%)' : 'translateX(0%)' }}
-                    >
-                        <div className={`signup ${isSignUp ? '' : 'nodisplay'}`}>
-                            <ProfileCard />
-                        </div>
-                        <div  className={`signin ${isSignUp ? 'nodisplay' : ''}`}>
-                           <BabyProfileCard />
-                        </div>
-                    </div>
-                    <div className='leftbox'>
-                        <h2 className="title">
-                            <span>Lovely Day </span><br></br>with <span>You</span>
-                        </h2>
-                        <p className="desc">Hello My <span>Baby</span></p>
-                        <img className='mx-auto my-12 w-32 h-32' src={baby1} alt='baby1'/>
-                        <button className='button' id='signin' onClick={handleSignInClick}>go to babyProfile</button>
-                    </div>
-                    <div className='rightbox'>
-                        <h2 className="title">
-                            <span>Always </span><br></br>With<span>You</span>
-                        </h2>
-                        <p className="desc">You make me <span>Happy!</span></p>
-                        <img className='mx-auto my-12 w-32 h-32' src={baby1} alt='baby1' />
-                        <button className="button" id="signup" onClick={handleSignUpClick}>go to userprofile</button>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="container">
+        <div className="welcome">
+          <div
+            className={`pinkbox ${isSignUp ? 'signup' : 'signin'}`}
+            style={{
+              transform: isSignUp ? 'translateX(80%)' : 'translateX(0%)',
+            }}
+          >
+            <div className={`signup ${isSignUp ? '' : 'nodisplay'}`}>
+              <ProfileCard />
             </div>
-           
+            <div className={`signin ${isSignUp ? 'nodisplay' : ''}`}>
+              <BabyProfileCard />
+            </div>
+          </div>
+          <div className="leftbox">
+            <h2 className="title">
+              <span>Lovely Day </span>
+              <br></br>with <span>You</span>
+            </h2>
+            <p className="desc">
+              Hello My <span>Baby</span>
+            </p>
+            <img className="mx-auto my-12 w-32 h-32" src={baby1} alt="baby1" />
+            <button className="button" id="signin" onClick={handleSignInClick}>
+              go to babyProfile
+            </button>
+          </div>
+          <div className="rightbox">
+            <h2 className="title">
+              <span>Always </span>
+              <br></br>With<span>You</span>
+            </h2>
+            <p className="desc">
+              You make me <span>Happy!</span>
+            </p>
+            <img className="mx-auto my-12 w-32 h-32" src={baby1} alt="baby1" />
+            <button className="button" id="signup" onClick={handleSignUpClick}>
+              go to userprofile
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  )
 }
-
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../redux/slices/authSlice'
 import { authApi } from '../axios'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { getBabyProfile } from '../redux/slices/babyProfileSlice'
 import LogButton from './buttons/LogButton'
 
 export default function Navbar() {
+  const { id } = useParams()
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
@@ -15,10 +16,6 @@ export default function Navbar() {
     (state) => state.babyProfile,
   )
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
-
-  const message = isLoggedIn
-    ? `오늘은 "${babyName}"의 어떤 모습을 기록해볼까요?`
-    : 'Welcome to Bebe Diary! Login Please!'
 
   useEffect(() => {
     if (isLoggedIn) {
@@ -49,14 +46,39 @@ export default function Navbar() {
     return <div>Error occurred!</div>
   }
 
+  ;<div className="px-20 py-2 rounded-full bg-[#1e1e1e]/5 ">
+    <p className="text-xl font-medium text-[#231f20] truncate">
+      Welcome to Bebe Diary! Login Please!
+    </p>
+  </div>
+
   return (
     <header className="flex justify-between items-center shadow-md p-2 gap-2 ">
       <Link to="/" className="shrink-0 mt-1">
         <h2 className="text-[#df6452] text-4xl">Bebe Diary</h2>
       </Link>
-      <div className="px-20 py-2 rounded-full bg-[#1e1e1e]/5 ">
-        <p className="text-xl font-medium text-[#231f20] truncate">{message}</p>
-      </div>
+      {isLoggedIn ? (
+        <nav className="flex justify-end gap-4 py-4">
+          <Link to="/new">
+            <span className="underline">New Diary</span>
+          </Link>
+          <Link to="/diaries">
+            <span className="underline">Diray List</span>
+          </Link>
+          <Link to={`/profile/${id}/edit`}>
+            <span className="underline">Profile Edit</span>
+          </Link>
+          <Link to={`/baby/${id}/register`}>
+            <span className="underline">Baby Profile register</span>
+          </Link>
+        </nav>
+      ) : (
+        <div className="px-20 py-2 rounded-full bg-[#1e1e1e]/5 my-2 ">
+          <p className="text-xl font-medium text-[#231f20] truncate">
+            Welcome to Bebe Diary! Login Please!
+          </p>
+        </div>
+      )}
       {isLoggedIn ? (
         <div className="flex items-center px-6 gap-6">
           <div className="w-16 h-16 rounded-full bg-slate-50 flex items-center object-cover">
